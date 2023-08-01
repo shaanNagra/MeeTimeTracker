17dac7a52a70600eb9109e16ac82f2299855c270
shaanNagra <shaan.n@outlook.co.nz>
2023-08-01 15:24:41 +1200

    loads countdownTimer widget into mainwindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,12 +1,25 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include "Timers/countdowntimer.h"
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    loadWidgets();
 }
+
+CountdownTimer *cTimer;
+
+void MainWindow::loadWidgets(){
+    QWidget *widge = ui->centralwidget;
+    cTimer = new CountdownTimer(widge);
+    cTimer->show();
+}
+
+
 
 MainWindow::~MainWindow()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -17,5 +17,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    void loadWidgets();
 };
 #endif // MAINWINDOW_H
